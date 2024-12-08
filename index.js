44950c63233787ccf60241a38ce865ae8bcc6933
Olek Semenets <bobrt440@gmail.com>
2024-12-08 20:20:59 +0100

    add editorconfig file, code formatting

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ const maxlength = parseInt(message.getAttribute('maxlength'), 10);
 message.addEventListener('input', onInput);
 
 function onInput(event) {
-  let length = event.target.value.length;
-  counter.innerText = maxlength - length + ' characters left…';
-  if (length >= maxlength) {
-    counter.innerHTML = `<span style='color:red;'>Max ${maxlength} characters!<\span>`
-  }
+    let length = event.target.value.length;
+    counter.innerText = maxlength - length + ' characters left…';
+    if (length >= maxlength) {
+        counter.innerHTML = `<span style='color:red;'>Max ${maxlength} characters!<\span>`
+    }
 }
 
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-  event.preventDefault();
-  form.removeEventListener('submit', onFormSubmit);
-  form.style.display = 'none';
-  reset.style.display = 'block';
-  reset.addEventListener('click', onReset);
+    event.preventDefault();
+    form.removeEventListener('submit', onFormSubmit);
+    form.style.display = 'none';
+    reset.style.display = 'block';
+    reset.addEventListener('click', onReset);
 }
 
 function onReset(event) {
-  reset.removeEventListener('click', onReset);
-  form.style.display = 'block';
-  reset.style.display = 'none';
-  form.addEventListener('submit', onFormSubmit);
+    reset.removeEventListener('click', onReset);
+    form.style.display = 'block';
+    reset.style.display = 'none';
+    form.addEventListener('submit', onFormSubmit);
 }
