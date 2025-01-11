58ebaf9ee25cff83cd479c3b7e30cfa294a999c0
Olek Semenets <bobrt440@gmail.com>
2025-01-11 17:49:21 +0100

    update github.js

--- github.js
+++ github.js
@@ -3,7 +3,9 @@ const gitLogin = document.getElementById('github-name')
 const gitBio = document.getElementById('github-bio')
 const gitLink = document.getElementById('github-link')
 const gitMainName = document.getElementById('git-main-name')
+const gitFooterName = document.getElementById('git-footer-name')
 const getAvatar = document.getElementById('avatar')
+const getTown = document.getElementById('git-town')
 
 
 fetch(url)
@@ -18,6 +20,8 @@ function gitData(data) {
     gitLink.setAttribute('href', data.html_url)
     gitLink.innerText = data.login
     gitMainName.innerText = data.name
+    gitFooterName.innerText = `Autor: ${data.name}`
     getAvatar.setAttribute('src', data.avatar_url)
     getAvatar.style.borderRadius = '50%'
+    getTown.innerHTML = `<strong>Town: </strong>${data.location}`
 }
