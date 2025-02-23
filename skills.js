2cac727e8cc6fafee623f1e3fafd9e1c2903e908
Oleksandr Semenets <150393074+semenetsoleksandr@users.noreply.github.com>
2025-02-23 16:28:13 +0100

    Adjust frontend to work with new api (#4)
    * add skills.json, implement loading skills.json to Home.html
    
    * add github.js, edit home page
    
    * update github.js
    
    * Implemented Caching in LocalStorage, Used Google Fonts, Used Font Awesome
    
    * del email, born
    
    * adjust frontend to work with new api
    
    * refactor

--- skills.js
+++ skills.js
@@ -1,42 +1,61 @@
 const formAddSkil = document.getElementById('add-skills')
 const newSkill = document.getElementById('new-skill')
 const skills = document.getElementById('skills')
-const form = document.getElementById('add-skills')
 
-
-fetch('./skills.json')
-    .then(response => response.json())
-    .then(el => {
-        el.skills.forEach((el) => {
-            const li = document.createElement('li')
-            const close = document.createElement('button')
-            close.setAttribute('type', 'button')
-            close.innerHTML = '<i class="fa-regular fa-circle-xmark fa-lg"></i>'
-            skills.appendChild(li)
-            li.innerHTML = `<span style="color:darkblue;" class="fa-li" ><i class="fa-solid fa-check-square"></i></span>${el} `
-            li.appendChild(close)
-            close.addEventListener('click', () => {
-                skills.removeChild(li)
-            })
-        });
-    })
-    .catch(error => console.error(error));
+function main() {
+    fetch('http://localhost:8080/skills')
+        .then(res => res.json())
+        .then(res => {
+            for (let skills in res) {
+                createListSkill(res[skills].skill, res[skills].id)
+            }
+        })
+        .catch(error => console.error(error));
+}
 
 formAddSkil.addEventListener('submit', addSkill)
 
-function addSkill(event) {
-    event.preventDefault();
-    if (newSkill.value != 0) {
-        let li = document.createElement('li')
-        let close = document.createElement('button')
-        close.setAttribute('type', 'button')
-        close.innerHTML = '<i class="fa-regular fa-circle-xmark fa-lg"></i>'
-        li.innerHTML = li.innerHTML = `<span style="color:darkblue;" class="fa-li"><i class="fa-solid fa-check-square"></i></span>${newSkill.value} `
-        skills.appendChild(li)
-        li.appendChild(close)
-        close.addEventListener('click', () => {
-            skills.removeChild(li)
-        })
-        newSkill.value = ''
+function addSkill() {
+    if (newSkill.value != "") {
+        fetch('http://localhost:8080/skills/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({"skill": newSkill.value})
+        });
+    } else {
+        alert("Add skill field cannot be empty")
     }
 }
+
+function createListSkill(data, id) {
+    const li = document.createElement('li')
+    skills.appendChild(li)
+    li.innerHTML = `<span style="color:darkblue;" class="fa-li" ><i class="fa-solid fa-check-square"></i></span>${data} `
+    clsBtn(li, id)
+}
+
+function clsBtn(li, id) {
+    const close = document.createElement('button')
+    close.setAttribute('type', 'button')
+    close.setAttribute('id', id)
+    close.innerHTML = '<i class="fa-regular fa-circle-xmark fa-lg"></i>'
+    li.appendChild(close)
+    close.addEventListener('click', () => {
+        skills.removeChild(li)
+        let id = close.getAttribute('id')
+        delSkill(id)
+    })
+}
+
+function delSkill(id) {
+    fetch(`http://localhost:8080/skills/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+    });
+}
+
+main()
